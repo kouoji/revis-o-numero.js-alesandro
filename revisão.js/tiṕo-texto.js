ca079fb339358c5e8f01b6d7d89f57e2865a592b
Alessandro-cesf <116165893+kouoji@users.noreply.github.com>
2024-03-13 12:33:10 -0300

    Add files via upload

---
+++ revisão.js/tiṕo-texto.js
@@ -0,0 +1,10 @@
+const nomeAluno = "Beatriz";
+const maeAluno = "Juliana";
+
+console.log(nomeAluno);
+
+const idadeAluno = 17;
+
+console.log(nomeAluno * idadeAluno);
+
+console.log(nomeAluno + maeAluno); //concatenação
