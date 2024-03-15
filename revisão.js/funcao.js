6c8d512950098e394d25cbb828ca40ede468e7e4
Alessandro-cesf <116165893+kouoji@users.noreply.github.com>
2024-03-15 12:11:32 -0300

    Add files via upload

---
+++ revisão.js/funcao.js
@@ -0,0 +1,63 @@
+/*função não parametriada
+
+const n1 = 8;
+const n2 = 5;
+
+function Soma() {
+  return n1 + n2;
+}
+
+function multiplica() {
+  return n1 * n2;
+}
+
+console.log(soma(), multiplica());
+*/
+
+//função parametrizada
+
+/*
+function soma(n3 = 0, n4 = 0) {
+  return n3 + n4;
+}
+
+console.log(soma(6, 5));
+*/
+
+/*funcão anônima
+
+const f1 = function (v1, v2) {
+  return v1 / v2;
+};
+
+console.log(f1(12, 25));
+*/
+
+/*vetores
+
+let mulheres = ["Poliana", "Camila", "Amanda"];
+
+console.log(mulheres);
+
+mulheres.map((el) => {
+  console.log(mulheres);
+});
+*/
+
+/*função construtora anonima
+
+const f2 = new Function("V3", "V4", "return v3-v4");
+
+console.log(f2(7, 3));
+
+*/
+
+/*
+const imc = function (PESO, ALTURA) {
+  return PESO / (ALTURA * ALTURA);
+};
+
+console.log(img(50, 1, 59));
+*/
+
+//com arrow function
---
+++ revisão.js/tipo-booleano.js
@@ -0,0 +1,9 @@
+let cidade = "Campo Largo";
+
+cidade = cidade.toLocaleLowerCase;
+
+let cidadeNatal = "campo largo";
+
+cidadeNatal = cidadeNatal.toLocaleLowerCase;
+
+console.log(cidade == cidadeNatal);
